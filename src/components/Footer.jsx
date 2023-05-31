48edffa277507d1800b1f0d0a0a773bb059c307b
Elman <115394943+ElmanSafarli@users.noreply.github.com>
2023-05-31 13:50:16 +0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,10 +11,7 @@ const Footer = () => {
             </div>
             <div className="footer-text">
                 <p>
-                    Sonxeber.az Azərbaycanın xəbər saytı, Azərbaycandan ən son xəbərləri
-                    yayımlayır. Iqtisadi xeberler, Şou biznes xəbərləri , siyasi xeberler, 
-                    kriminal xeberler, idman xeberleri, muharibe xeberleri, maliyye xeberleri, 
-                    son xeberler təqdim edən ictimai-sosial saytdır. xeberler bu gun 2022
+                    Xarici və ölkə mediyasından informasiyalar göturülə bilər
                 </p>
             </div>
         </div>
